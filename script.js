d3d25ad2ee758a7496f62dcd24707fc573961fea
e-n-p <nickellam0@gmail.com>
2024-04-07 20:49:45 +0200

    Adding dynamic loading on page of video data

--- script.js
+++ script.js
@@ -7,5 +7,27 @@ function fetchVideoData() {
         })
         .then((videoData) => {
             console.log(videoData);
+            CreateVideoCardContainer(videoData.Videos)
         })
-}
+};
+
+
+function CreateVideoCardContainer(videoData){
+    const videoEl = document.querySelector('.video-section');
+    for (let video of videoData) {
+        videoEl.innerHTML += `
+        <div class="card">
+            <div class="thumbnail">
+                <img src=${video.Thumbnail}>
+            </div>
+            <div class="details">
+                <p>${video.Title}</p>
+                <p>${video.User}</p>
+                <p>${video.Views}</p>
+                <p>${video.PostDate}</p>
+                <p>${video.RunTime}</p>
+            </div>
+        </div>
+    `
+    }
+};
